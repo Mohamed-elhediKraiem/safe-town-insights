980dbae513341eaf9024e662d611552cb5b5754d
Mohamed-elhediKraiem <113349583+Mohamed-elhediKraiem@users.noreply.github.com>
2025-09-23 18:05:38 +0200

    fix : correction path file

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -8,25 +8,26 @@ import os
 # base path = dossier courant du script
 BASE_DIR = os.path.dirname(__file__)
 
+
 # Chargement des fichiers avec chemins relatifs robustes
 df_crimes = pd.read_csv(
-    os.path.join(BASE_DIR, '..', 'data', 'dimension_crime.csv'),
-    sep=';', encoding='latin1'
+    os.path.join(BASE_DIR, '..', '..', 'data', 'dimension_crime.csv'),
+    sep=';', #encoding='latin1'
 )
 
 df_departements_without_central = pd.read_csv(
-    os.path.join(BASE_DIR, '..', 'data', 'dimension_departement_enrichi2.csv'),
-    sep=';', encoding='latin1'
+    os.path.join(BASE_DIR, '..', '..', 'data', 'dimension_departement_enrichi2.csv'),
+    sep=';', #encoding='latin1'
 )
 
 df_departements_central = pd.read_csv(
-    os.path.join(BASE_DIR, '..', 'data', 'points-extremes-des-departements-metropolitains-de-france.csv'),
-    sep=',', encoding='latin1'
+    os.path.join(BASE_DIR, '..', '..', 'data', 'points-extremes-des-departements-metropolitains-de-france.csv'),
+    sep=',',# encoding='latin1'
 )
 
 df_resultat = pd.read_csv(
-    os.path.join(BASE_DIR, '..', 'data_cleaning', 'resultat.csv'),
-    sep=';', encoding='latin1'
+    os.path.join(BASE_DIR, '..',  'data_cleaning', 'resultat.csv'),
+    sep=';', #encoding='latin1'
 )
 
 
